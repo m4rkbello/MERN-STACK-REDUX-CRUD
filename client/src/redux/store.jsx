5e72b931063e0eb8fabf3a8cb3954466cb98dba9
m4rkbello <markbello10041999@gmail.com>
2024-01-21 20:11:55 +0800

    SETUP REDUX AND INSTALL BOOTSTRAP

---
+++ client/src/redux/store.jsx
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+import userReducer from './useSlice';
+
+const store = configureStore({
+    reducer: {
+        users: userReducer
+    }
+})
+
+export default store;
