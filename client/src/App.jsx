dd6c398c340394725747682c8b7ec97750f0c8c7
m4rkbello <markbello10041999@gmail.com>
2024-01-25 00:36:08 +0800

    NAAY BUGS

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,38 @@
 import './App.css'
+import axios from 'axios'
+import { useEffect } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Users from './Users'
 import CreateUser from './CreateUser'
 import UpdateUser from './UpdateUser'
+import { useDispatch} from 'react-redux'
+import { getUser } from './redux/userSlice'
 
 
 function App() {
 
+    //dispatch
+    const dispatch = useDispatch()
+  
+    //useSelector kuhaon ang data
+    // const users = useSelector(state => state.users.users)
+    // console.log("HOY",useSelector(state => state.users.users));
+
+
+  //pagfetch ug data gikan sa BACKEND
+useEffect(() => {
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('http://127.0.0.1:3000');
+      console.log("Axios Response:", response);
+      dispatch(getUser(response.data));
+    } catch (err) {
+      console.error("AxiosError:", err.message);
+    }
+  };
+
+  fetchData();
+}, []);
 
   return (
     <div>
@@ -14,7 +40,7 @@ function App() {
         <Routes>
           <Route exact path='/' element={<Users />} />
           <Route exact path='/create' element={<CreateUser />} />
-          <Route exact path='/update' element={<UpdateUser />} />
+          <Route exact path='/update/:id' element={<UpdateUser />} />
         </Routes>
       </Router>
     </div>
